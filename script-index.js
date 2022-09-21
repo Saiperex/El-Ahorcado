423b37e58aed5d61b74ab3da8bcc6b1565ad1d4d
Xavier <xaviramos668@gmail.com>
2022-09-21 13:17:12 -0300

    Responsive

--- script-index.js
+++ script-index.js
@@ -10,13 +10,21 @@ function ocultarNombre()
     if(texto.trim().length>0)/*Lo que hace es eliminar los espacios en blanco */
     {
         document.getElementById("nombre-jugador").style.display="none";
+        document.getElementById("tipo-juego").style.display="grid";
     }
     else
     {
         alerta.textContent="Te crees muy listo? Escribe tu nombre";
         document.getElementById("alert").style.display="grid";
+        document.getElementById("nombre").value="";
     }
     
+}
+function ocultarAcerca()
+{
+    var alerta= document.getElementById("texto");
+    alerta.textContent="Juego desarrolado por Xavier Medina. El Ahrocado. Proyecto realizado Para el Challenge II de Oracle One en colaboracion con Alura Latam";
+    document.getElementById("alert").style.display="grid";
 }
 function ocultaralerta()
 {
